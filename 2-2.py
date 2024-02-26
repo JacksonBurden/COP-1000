c72a9c4b51677e9c5895c29704270bc52041dd73
JacksonBurden <159358587+JacksonBurden@users.noreply.github.com>
2024-02-25 21:56:29 -0500

    Add files via upload

---
+++ 2-2.py
@@ -0,0 +1,6 @@
+myCurrentAge = input ("Enter your current age:")
+value = int(myCurrentAge)
+currentYear = 2024
+Diff = 2050 - currentYear
+calculatedAge = Diff + value
+print (f"Your age in 2050 will be: {calculatedAge}")
